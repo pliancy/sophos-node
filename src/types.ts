f9506e026e2135dce4547ce5887719d3f6ee6287
santese <43831817+santese@users.noreply.github.com>
2021-02-19 14:25:12 -0800

    Removed optional properties from Types

--- src/types.ts
+++ src/types.ts
@@ -5,77 +5,77 @@ export interface SophosConfig {
 }
 
 export interface SophosTenant {
-  id?: string
-  name?: string
-  dataGeography?: string
-  dataRegion?: string
-  billingType?: string
-  partner?: object
-  organization?: string
-  apiHost?: string
+  id: string
+  name: string
+  dataGeography: string
+  dataRegion: string
+  billingType: string
+  partner: object
+  organization: string
+  apiHost: string
 }
 
 export interface SophosEndpoint {
-  id?: string
-  type?: string
-  tenant?: {
-    id?: string
+  id: string
+  type: string
+  tenant: {
+    id: string
   }
-  hostname?: string
-  health?: {
-    overall?: string
-    threats?: {
-      status?: string
+  hostname: string
+  health: {
+    overall: string
+    threats: {
+      status: string
     }
-    services?: {
-      status?: string
-      serviceDetails?: SophosEndpointService[]
+    services: {
+      status: string
+      serviceDetails: SophosEndpointService[]
     }
   }
-  os?: {
-    isServer?: boolean
-    platform?: string
-    name?: string
-    majorVersion?: number
-    minorVersion?: number
-    build?: number
+  os: {
+    isServer: boolean
+    platform: string
+    name: string
+    majorVersion: number
+    minorVersion: number
+    build: number
   }
-  ipv4Addresses?: string[]
-  macAddresses?: string[]
-  associatedPerson?: {
-    name?: string
-    viaLogin?: string
-    id?: string
+  ipv4Addresses: string[]
+  macAddresses: string[]
+  associatedPerson: {
+    name: string
+    viaLogin: string
+    id: string
   }
-  tamperProtectionEnabled?: boolean
-  assignedProducts?: SophosEndpointServiceAssignedProduct[]
-  lastSeenAt?: string
-  encryption?: {
-    volumes?: SophosEndpointEncryptionVolume[]
+  tamperProtectionEnabled: boolean
+  assignedProducts: SophosEndpointServiceAssignedProduct[]
+  lastSeenAt: string
+  encryption: {
+    volumes: SophosEndpointEncryptionVolume[]
   }
 }
 
 export interface SophosEndpointService {
-  name?: string
-  status?: string
+  name: string
+  status: string
 }
 
 export interface SophosEndpointServiceAssignedProduct {
-  code?: string
-  version?: string
-  status?: string
+  code: string
+  version: string
+  status: string
 }
 export interface SophosEndpointEncryptionVolume {
-  volumeId?: string
-  status?: string
+  volumeId: string
+  status: string
 }
 
 export interface SophosAccessToken {
-  access_token?: string
-  errorCode?: string
-  expires_in?: string
-  message?: string
-  refresh_token?: string
-  token_type?: string
-  trackingId?: string
+  access_token: string
+  errorCode: string
+  expires_in: string
+  message: string
+  refresh_token: string
+  token_type: string
+  trackingId: string
 }
