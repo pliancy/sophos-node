8b82bba2c8520f8994c8439a9f605bb0ea8f4f49
santese <43831817+santese@users.noreply.github.com>
2021-02-19 01:54:08 -0800

    Updated Tests for TS

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,12 @@
-it('does nothing for now', () => {
-  expect(true).toEqual(true)
+import { Sophos } from '.'
+
+describe('TimelyApp class', () => {
+  it('creates a TimelyApp instance', () => {
+    const sophos = new Sophos({
+      clientId: '1234',
+      clientSecret: 'asdf',
+    })
+
+    expect(sophos).toBeInstanceOf(Sophos)
+  })
 })
